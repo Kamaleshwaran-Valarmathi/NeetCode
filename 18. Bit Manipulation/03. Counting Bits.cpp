fe3a0cb86f0cb48b514c4811ee313c4c89e95225
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2024-05-12 12:41:15 +0530

    Create 03. Counting Bits.cpp

---
+++ 18. Bit Manipulation/03. Counting Bits.cpp
@@ -0,0 +1,81 @@
+// Reference: https://leetcode.com/problems/counting-bits/
+
+
+
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+// ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+// Example 1:
+// ==========
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+
+// Example 2:
+// ==========
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+ 
+
+
+// Constraints:
+// ============
+// 0 <= n <= 10^5
+ 
+
+
+// Follow up:
+// - It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in 
+//      linear time O(n) and possibly in a single pass?
+// - Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+
+
+
+
+
+// Approach 1
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n+1);
+        for (int i = 1; i <= n; ++i)
+            res[i] = res[i & (i-1)] + 1;
+        return res;
+    }
+};
+
+
+
+
+
+// Approach 2
+
+class Solution {
+public:
+    int counter(int n) {
+        int count;
+        for (count = 0; n; n &= (n-1))
+            ++count;
+        return count;
+    }
+
+    vector<int> countBits(int n) {
+        vector<int> res(n+1);
+        for (int i = 0; i <= n; ++i)
+            res[i] = counter(i);
+        return res;
+    }
+};
