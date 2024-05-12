e85dcd42f513784e150a2f1e27d98ecdc686a5b1
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2024-05-12 12:09:37 +0530

    Create 10. Graph Valid Tree.cpp

---
+++ 11. Graphs/10. Graph Valid Tree.cpp
@@ -0,0 +1,60 @@
+// Reference: https://leetcode.ca/2016-08-17-261-Graph-Valid-Tree/
+
+
+
+// Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of 
+// nodes), write a function to check whether these edges make up a valid tree.
+
+// Note: you can assume that no duplicate edges will appear in edges. Since all edges are 
+// undirected, [0,1] is the same as [1,0] and thus will not appear together in edges
+
+
+
+// Example 1:
+// ==========
+// Input: n = 5, and edges = [[0,1], [0,2], [0,3], [1,4]]
+// Output: true
+
+// Example 2:
+// ==========
+// Input: n = 5, and edges = [[0,1], [1,2], [2,3], [1,3], [1,4]]
+// Output: false
+
+ 
+
+// Constraints:
+// ============
+// 1 <= n <= 2000
+// 0 <= edges.length <= 5000
+// edges[i].length == 2
+// 0 <= ai, bi < n
+// ai != bi
+// There are no self-loops or repeated edges.
+
+
+
+
+
+class Solution {
+public:
+    int find(int node, vector<int>& parent) {
+        if (parent[node] != node)
+            parent[node] = find(parent[node], parent);
+        return parent[node];
+    }
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n);
+        for (int i = 0; i < n; ++i)
+            parent[i] = i;
+        for (vector<int> edge: edges) {
+            int parentA = find(edge[0], parent);
+            int parentB = find(edge[1], parent);
+            if (parentA == parentB)
+                return false;
+            parent[parentA] = parentB;
+            --n;
+        }
+        return (n == 1);
+    }
+};
