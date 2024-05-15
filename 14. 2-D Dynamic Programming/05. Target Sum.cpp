af18b8160e850ad30edc714fe96695c0a10853de
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2024-05-15 10:36:31 +0530

    Create 05. Target Sum.cpp

---
+++ 14. 2-D Dynamic Programming/05. Target Sum.cpp
@@ -0,0 +1,62 @@
+// Reference: https://leetcode.com/problems/target-sum/
+
+
+
+// You are given an integer array nums and an integer target.
+
+// You want to build an expression out of nums by adding one of the symbols '+' and '-' before 
+// each integer in nums and then concatenate all the integers.
+
+// For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate 
+// them to build the expression "+2-1".
+
+// Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+// Example 1:
+// ==========
+// Input: nums = [1,1,1,1,1], target = 3
+// Output: 5
+// Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+// -1 + 1 + 1 + 1 + 1 = 3
+// +1 - 1 + 1 + 1 + 1 = 3
+// +1 + 1 - 1 + 1 + 1 = 3
+// +1 + 1 + 1 - 1 + 1 = 3
+// +1 + 1 + 1 + 1 - 1 = 3
+
+// Example 2:
+// ==========
+// Input: nums = [1], target = 1
+// Output: 1
+ 
+
+
+// Constraints:
+// ============
+// 1 <= nums.length <= 20
+// 0 <= nums[i] <= 1000
+// 0 <= sum(nums[i]) <= 1000
+// -1000 <= target <= 1000
+
+
+
+
+
+class Solution {
+public:
+    int helper(int idx, int sum, vector<int>& nums, int target, map<pair<int, int>, int>& dp) {
+        if (idx == nums.size())
+            return (sum == target) ? 1 : 0;
+        if (dp.find({idx, sum}) != dp.end())
+            return dp[{idx, sum}];
+        int positive = helper(idx+1, sum+nums[idx], nums, target, dp);
+        int negative = helper(idx+1, sum-nums[idx], nums, target, dp);
+        return dp[{idx, sum}] = positive + negative;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        map<pair<int, int>, int> dp;
+        return helper(0, 0, nums, target, dp);
+    }
+};
