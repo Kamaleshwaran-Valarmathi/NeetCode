b7cb15c878b0d3fb9248edb2cc6ae5b103659ede
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2024-05-22 08:14:34 +0530

    Create 09. Reverse String.cpp

---
+++ 02. Two Pointers/09. Reverse String.cpp
@@ -0,0 +1,38 @@
+// Reference: https://leetcode.com/problems/reverse-string/
+
+
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+// ==========
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+// Example 2:
+// ==========
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+
+// Constraints:
+// ============
+// 1 <= s.length <= 10^5
+// s[i] is a printable ascii character.
+ 
+
+
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& str) {
+        for (int left = 0, right = str.size()-1; left < right; ++left, --right)
+            swap(str[left], str[right]);
+    }
+};
